0b6826646e336bc3396907de8abdd3055f61c317
sebastianbertoli <bertoli.sebastian@gmail.com>
2017-05-31 13:32:58 +0200

    del

--- old/_dump/create missing msi.R
+++
@@ -1,8 +0,0 @@
-
-tmp_mmsil <- data.frame(mmsi = as.integer(unique(df_raw$MMSI)))
-tmp_mmsir <- data.frame(mmsi = as.integer(unique(df_vesseldetails$MMSI)))
-
-missing_mmsi <- anti_join(tmp_mmsil,tmp_mmsir, by = 'mmsi') 
-
-fn_export_object(missing_mmsi)
-fn_rmobj('tmp')
--- old/_dump/export to cartoDB.R
+++
@@ -1,9 +0,0 @@
-# Select a random sample of 500.000 vessels
-set.seed(1234)
-df_vessel3M <- sample_n(df_1[,c("LAT",'LON')], size = 3000000)
-fn_export_object(df_vessel3M)
-rm(df_vessel3M)
-
-
-df_vessel100k <- sample_n(df_1[,c("LAT",'LON','TIMESTAMP')], size = 100000)
-fn_export_object(df_vessel100k)
